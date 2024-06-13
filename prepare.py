d5e6b63a9ee0891b585ea68e18e7db58c2ba8a06
Dimitri Podborski <podborski@users.noreply.github.com>
2024-06-13 13:31:00 -0700

    organize

--- prepare.py
+++ prepare.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 
 
 def merge_bikeshed_files(directory, output_file):
@@ -7,15 +8,13 @@ def merge_bikeshed_files(directory, output_file):
     files = [f for f in os.listdir(directory) if f.endswith('.bs') and file_pattern.match(f)]
     files.sort()
 
-    files.insert(0, 'index_header.bs')
+    print(f'Merge {len(files)} markdown files into {output_file}')
 
     with open(output_file, 'w') as outfile:
         for filename in files:
-            print(f'Merge {filename}')
             filepath = os.path.join(directory, filename)
             with open(filepath, 'r') as infile:
                 outfile.write(infile.read())
-                # outfile.write('\n\n')
 
 
 if __name__ == "__main__":
@@ -23,3 +22,6 @@ if __name__ == "__main__":
     output_bs_filename = 'index.bs'
 
     merge_bikeshed_files(directory, output_bs_filename)
+
+    print('Copy footer.include file')
+    shutil.copyfile(os.path.join(directory, 'footer.include'), './footer.include')
